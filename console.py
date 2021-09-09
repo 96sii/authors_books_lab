14aa7a35849c3e0a42d072d761d2d07f04a95b69
Jane McNab <writetojaney@hotmail.co.uk>
2021-09-09 12:55:21 +0100

    finishes mvp and some extensions

--- console.py
+++ console.py
@@ -9,9 +9,13 @@ author_repository.delete_all()
 book_repository.delete_all()
 
 author_1 = Author('Stephen', 'King')
+author_2 = Author('Frank', 'Herbert')
 author_repository.save(author_1)
+author_repository.save(author_2)
 
 book_1 = Book("IT", author_1, "Viking", "19/09/1986")
+book_2 = Book("Dune", author_2, "Chilton Books", "19/08/1965")
 book_repository.save(book_1)
+book_repository.save(book_2)
 
 pdb.set_trace()
